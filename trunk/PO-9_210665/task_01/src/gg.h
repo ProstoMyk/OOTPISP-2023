3d6876cf7ccbb7f14819afc3c7288a059e9be01a
ProstoMyk <strausiskusta@gmail.com>
2023-10-13 10:12:19 +0300

    task_01

---
+++ trunk/PO-9_210665/task_01/src/gg.h
@@ -0,0 +1,40 @@
+#ifndef GG_H
+#define GG_H
+#include <QMainWindow>
+#include "QPushButton"
+#include "QSpinBox"
+#include "QLabel"
+#include "QPlainTextEdit"
+#include "QTextEdit"
+#include "QSize"
+#include "QStyle"
+#include "QCommonStyle"
+#include "QFormLayout"
+#include "QPlainTextEdit"
+#include "QFileDialog"
+#include "QFile"
+#include "QString"
+#include "QByteArray"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class gg; }
+QT_END_NAMESPACE
+
+class gg : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    gg(QWidget *parent = nullptr);
+    ~gg();
+    QPushButton *but;
+
+    QPlainTextEdit* txtEdit;
+
+private:
+    Ui::gg *ui;
+
+public slots:
+    void Otkr();
+};
+#endif // GG_H
