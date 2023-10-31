136cf4bd29c167b6b0de37531de33059848cc21e
ProstoMyk <strausiskusta@gmail.com>
2023-10-31 15:54:06 +0300

    [task_03] maina mne ne hvatalo

--- trunk/PO-9_210665/task_03/src/main.cpp
+++ trunk/PO-9_210665/task_03/src/main.cpp
@@ -1,11 +1,11 @@
-#include "mainwindow.h"
+#include "mainwindow.h"//210665_3
 
-#include <QApplication>
+#include <QApplication>//210665_3
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
+int main(int argc, char *argv[])//210665_3
+{//210665_3
+    QApplication a(argc, argv);//210665_3
+    MainWindow w;//210665_3
+    w.show();//210665_3
+    return a.exec();//210665_3
+}//210665_3
