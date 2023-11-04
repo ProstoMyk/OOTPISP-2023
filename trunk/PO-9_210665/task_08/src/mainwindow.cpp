c5bab44d9b4d0573adcece096cf70f570718589f
ProstoMyk <strausiskusta@gmail.com>
2023-11-04 21:54:34 +0300

    ++

--- trunk/PO-9_210665/task_08/src/mainwindow.cpp
+++ trunk/PO-9_210665/task_08/src/mainwindow.cpp
@@ -4,7 +4,7 @@
 MainWindow::MainWindow(QWidget *parent) ////210665_8
     : QMainWindow(parent) ////210665_8
     , ui(new Ui::MainWindow) //210665_8
-{
+{//210665_888
     ui->setupUi(this); //210665_8
     ui->groupBox->setLayout(new QVBoxLayout(this));//210665_8
     ui->groupBox->layout()->addWidget(new CustomWidget(this));//210665_8
